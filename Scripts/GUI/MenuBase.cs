03969b2a204b978694efc0cc498e4aad572f2f91
Juan <diclonius32@gmail.com>
2018-09-03 22:13:15 -0500

    * Added Menu utils * Added Event Managers.

---
+++ Scripts/GUI/MenuBase.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using GameEventSystem;
+
+public class MenuBase : MonoBehaviour {
+
+    public void onEnterScreen()
+    {
+        // Do basic logic for opening
+        preActive();
+        InitScreen();
+        Canvas aCanvas = gameObject.GetComponent<Canvas>();
+        aCanvas.worldCamera = Camera.main;
+        
+        gameObject.SetActive(true);
+        postActive();
+    }
+
+    public virtual void onExitScreen()
+    {
+        // Do basic logic for closing
+        preDeactive();
+        gameObject.SetActive(false);
+    }
+
+	public void ShowMenu(int menu, bool closePrivous = true)
+    {
+        Hashtable hashParams = new Hashtable();
+        hashParams.Add(GameEventParam.SCREEN_TO_OPEN, menu);
+
+        GameEventManager.TriggerEvent(GameEvents.E_OPEN_SCREEN, hashParams);
+    }
+
+    public void SetCloseButton(Button closeButton)
+    {
+        closeButton.onClick.AddListener(OnCloseButtonClick);
+    }
+    
+    public void OnCloseButtonClick()
+    {
+        onExitScreen();
+    }
+
+	public virtual void InitScreen () {}
+    public virtual void preActive  () {}
+    public virtual void postActive () {}
+    public virtual void preDeactive() {}
+}
---
+++ Scripts/GUI/MenuManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using GameEventSystem;
+
+public class MenuManager : MonoBehaviour {
+
+	public static MenuManager Ins;
+
+	// Menus
+    public static int M_MAIN_MENU 	    = 0;
+	public static int M_PAUSE 		    = 1;
+	public static int M_SETTINGS 	    = 2;
+    public static int M_GAME_OVER       = 4;
+    
+    private Dictionary<int, string> menuPrefabs;
+
+    private int currentScreenEntry;
+    private GameObject currentScreen;
+
+    private string menusPath = "GUI/MenuPrefabs/";
+
+    public void CreateMenuDic()
+    {
+        menuPrefabs = new Dictionary<int,string>()
+		{
+			{ M_MAIN_MENU,          menusPath + "MainMenu"          },
+            { M_PAUSE,              menusPath + "PauseMenu"         },
+            { M_SETTINGS,           menusPath + "SettingsMenu"      },
+            { M_GAME_OVER,          menusPath + "GameOverMenu"      },
+		};	
+    }
+
+    private void Awake()
+    {
+        Ins = this;
+        GameEventManager.StartListening(GameEvents.E_OPEN_SCREEN, OnOpenScreenRequest);
+        MenuManager.Ins.CreateMenuDic();
+    }
+
+	void OnOpenScreenRequest(Hashtable param)
+    {
+        if(param.ContainsKey(GameEventParam.SCREEN_TO_OPEN))
+        { 
+            int screen = (int)param[GameEventParam.SCREEN_TO_OPEN];
+            ShowMenu(screen);
+        }
+    }
+
+	public void ShowMenu(int menu, bool hideMenuFlag = true)
+    {        
+        GameObject desiredScreen;
+		string prefabLocation = menuPrefabs[menu];
+		desiredScreen = (GameObject)Instantiate(Resources.Load(prefabLocation, typeof(GameObject)));
+		showMenu(ref desiredScreen, hideMenuFlag);
+		currentScreenEntry = menu;
+        
+    }
+
+	private void showMenu(ref GameObject menu, bool hideMenuFlag = true)
+    {
+        MenuBase aCurrentBase = currentScreen != null ? currentScreen.GetComponent<MenuBase>() : null;
+        
+		if (aCurrentBase)
+        {
+            aCurrentBase.onExitScreen();
+
+            if (menuPrefabs.ContainsKey(currentScreenEntry))
+			{
+				Object.DestroyImmediate(currentScreen, true);
+			}
+        }
+
+        if (currentScreen && hideMenuFlag)
+		{
+			hideMenu(currentScreen);
+		}
+            
+        Trace.Log("MainMenu - Show Menu:" + menu.name);
+        
+		currentScreen = menu;
+        MenuBase aBase = menu.GetComponent<MenuBase>();
+        if (aBase)
+		{
+			aBase.onEnterScreen();
+		}
+        else
+        {
+            Trace.Log("Error, component not found, proceeding the old way...");
+            menu.SetActive(true);
+        }
+    }
+
+    private void hideMenu(GameObject menu)
+    {
+        Trace.Log("MainMenu - Hide Menu: " + menu.name);
+        menu.SetActive(false);
+    }
+}

49787828500532cb1e05dc51c79261822bacef78
Juan Vazquez <diclonius32@gmail.com>
2019-04-11 14:04:38 -0500

    Added GameEvents and GameEventParam templates

---
+++ Scripts/Events/GameEventParam.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEventParam : MonoBehaviour
+{
+    // Menus
+    
+    public static string SCREEN_TO_OPEN = "ScreenToOpen";
+}
---
+++ Scripts/Events/GameEvents.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    // Menus
+    
+    public static string E_OPEN_SCREEN = "OpenScreen";
+
+}

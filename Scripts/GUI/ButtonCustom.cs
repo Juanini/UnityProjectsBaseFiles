66d7c446a8dce0727dea519d9c1b8c82fb4f5a5a
Juan Vazquez <diclonius32@gmail.com>
2019-04-27 20:58:12 -0500

    Added Custom button script

---
+++ Scripts/GUI/ButtonCustom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+using Sirenix.OdinInspector;
+using TMPro;
+
+public class ButtonCustom : MonoBehaviour
+{
+    [BoxGroup("Elements")] public TextMeshProUGUI buttonTextLabel;
+
+    [BoxGroup("Properties")] public string buttonTextStr;
+
+    private Button button;
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+        buttonTextLabel.text = buttonTextStr;
+    }
+
+    public void SetAction(UnityAction action)
+    {
+        button.onClick.AddListener(action);
+    }
+}

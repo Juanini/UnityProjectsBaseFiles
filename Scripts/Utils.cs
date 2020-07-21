1e7d7939946ff77fc6e270624afe65daada15fe4
Juan Vazquez <diclonius32@gmail.com>
2020-07-21 15:51:29 -0500

    * Utils Update

--- Scripts/Utils.cs
+++ Scripts/Utils.cs
@@ -19,4 +19,15 @@ public class Utils : MonoBehaviour
         
         return false;
     }
+
+    public static void ShuffleIntList(ref List<int> _list)
+    {
+        for (int i = 0; i < _list.Count; i++) 
+        {
+            int temp = _list[i];
+            int randomIndex = Random.Range(i, _list.Count);
+            _list[i] = _list[randomIndex];
+            _list[randomIndex] = temp;
+        }
+    }
 }

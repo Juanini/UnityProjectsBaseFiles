b0eca7a646ae30c1437ea2341688873f8c686c49
Juan Vazquez <diclonius32@gmail.com>
2022-06-20 12:17:28 -0500

    * Progress Bar new methods added

--- Scripts/GUI/ProgressBar.cs
+++ Scripts/GUI/ProgressBar.cs
@@ -9,6 +9,10 @@ using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
+    [Header("Options")]
+    public bool hideText = false;
+    
+    [Header("Elements")]
     public GameObject bar;
     public GameObject startPos;
     public GameObject completePos;
@@ -19,15 +23,22 @@ public class ProgressBar : MonoBehaviour
 
     private float incrementValue;
 
-    void Start()
+    private void Awake()
     {
         incrementValue = (completePos.transform.localPosition.x - startPos.transform.localPosition.x) / maxValue;
         Trace.Log(this.name + " - " + "IncrementValue = " + incrementValue);
+
+        if (hideText)
+        {
+            valueText.gameObject.SetActive(false);
+        }
     }
     
     [Button(ButtonSizes.Large), GUIColor(0.4f, 0.8f, 1)]
     public void SetupBar(int _maxValue)
     {
+        Trace.Log("Progress Bar - SetupBar - Max Value: " + _maxValue);
+        
         DOTween.Kill(bar.transform);
         maxValue = _maxValue;
         incrementValue = (completePos.transform.localPosition.x - startPos.transform.localPosition.x) / maxValue;
@@ -37,7 +48,8 @@ public class ProgressBar : MonoBehaviour
     [Button(ButtonSizes.Large), GUIColor(0.4f, 0.8f, 1)]
     public void DoValueBalance( float _value,
         float _time,
-        UnityAction _updateCallback = null)
+        UnityAction _updateCallback = null,
+        Ease _ease = Ease.InOutQuint)
     {
         if(_value > maxValue) { return; }
 
@@ -47,9 +59,37 @@ public class ProgressBar : MonoBehaviour
             .SetEase(Ease.Linear);
 
         bar.transform.DOLocalMove(new Vector3(bar.transform.localPosition.x + (incrementValue * (_value - value)), 0, 0), _time)
-            .SetEase(Ease.InOutQuint);
+                     .SetEase(_ease);
 
         updateCall = _updateCallback;
+    }
+    
+    [Button(ButtonSizes.Large), GUIColor(0.4f, 0.8f, 1)]
+    public void DoValueIncrement( float _value,
+        float _time,
+        UnityAction _updateCallback = null,
+        Ease _ease = Ease.InOutQuint)
+    {
+        if(value + _value > maxValue) { return; }
+
+        DOTween.To(()=> value, x=> value = x, value + _value, _time)
+            .OnUpdate(OnValueUpdate)
+            .OnComplete(OnValueComplete)
+            .SetEase(Ease.Linear);
+
+        bar.transform.DOLocalMove(new Vector3(bar.transform.localPosition.x + (incrementValue * (_value - value)), 0, 0), _time)
+            .SetEase(_ease);
+
+        updateCall = _updateCallback;
+    }
+
+    public void SetValue(int _value)
+    {
+        Trace.Log("Progress Bar - SetValue - Max Value: " + _value);
+        
+        if(_value > maxValue) { return; }
+        
+        bar.transform.localPosition = new Vector3(bar.transform.localPosition.x + (incrementValue * (_value - value)), 0, 0);
     }
     
     public void ShowText()

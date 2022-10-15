1a89c58e566d05a73e977fef72f50189cce825e2
Juan Vazquez <diclonius32@gmail.com>
2022-10-15 13:17:59 -0500

    + Collision detector added

---
+++ Scripts/CollisionDetector.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CollisionDetector : MonoBehaviour
+{
+    private UnityAction onEnterCallback;
+    private string onEnterTag;
+    private UnityAction onExitCallback;
+
+    public void Setup(
+        UnityAction _onEnterCallback,
+        UnityAction _onExitCallback,
+        string _tag)
+    {
+        onEnterTag = _tag;
+        onEnterCallback = _onEnterCallback;
+        onExitCallback = _onExitCallback;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag(onEnterTag))
+        {
+            onEnterCallback?.Invoke();    
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(onEnterTag))
+        {
+            onExitCallback?.Invoke();
+        }
+    }
+}

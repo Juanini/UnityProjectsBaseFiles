aaa4d6afe117784b286897c871f7d3d9b8dc9d0b
Juan Vazquez <diclonius32@gmail.com>
2023-04-06 18:48:44 -0600

    * Spine Animation added

---
+++ Scripts/Anim/SpineAnimData.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using Spine;
+using Spine.Unity;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Anim", fileName = "SpineAnimData", order = 0)]
+[InlineEditor]
+public class SpineAnimData : ScriptableObject
+{
+    public List<StateNameToAnimationReference> statesAndAnimations = new List<StateNameToAnimationReference>();
+    
+    // Alternately, an AnimationPair-Animation Dictionary (commented out) can be used for more efficient lookups.
+    public List<AnimationTransition> transitions = new List<AnimationTransition>();
+    
+    // [ShowInInspector]
+    // public Dictionary<Spine.AnimationStateData.AnimationPair, Spine.Animation> transitionDictionary 
+    //     = new Dictionary<AnimationStateData.AnimationPair, Spine.Animation>(Spine.AnimationStateData.AnimationPairComparer.Instance);
+    
+    [System.Serializable]
+    public class StateNameToAnimationReference 
+    {
+        public string stateName;
+        public AnimationReferenceAsset animation;
+    }
+
+    [System.Serializable]
+    public class AnimationTransition 
+    {
+        public AnimationReferenceAsset from;
+        public AnimationReferenceAsset transition;
+        public AnimationReferenceAsset to;
+    }
+}
--- Scripts/GUI/DialogMenu.cs
+++ Scripts/GUI/DialogMenu.cs
@@ -30,92 +30,92 @@ namespace HannieEcho
 
         private readonly Vector3 punchV = new Vector3(0.35f, 0.35f, 0.35f);
 
-        public async UniTask ShowDialog(string _text, bool _enableNext = true, int _position = GameConst.POS_UP)
-        {
-            nextImage.gameObject.SetActive(false);
-            nextButton.gameObject.SetActive(false);
-            nextButtonFullScreen.gameObject.SetActive(false);
-            
-            switch (_position)
-            {
-                case GameConst.POS_UP:
-                    container.transform.position = posUp.transform.position;
-                    break;
-            }
-            
-            container.gameObject.SetActive(true);
-            container.transform.DOPunchScale(punchV, 0.23f);
-            await SetText(_text);
-            
-            await UniTask.Delay(500);
+        // public async UniTask ShowDialog(string _text, bool _enableNext = true, int _position = GameConst.POS_UP)
+        // {
+        //     nextImage.gameObject.SetActive(false);
+        //     nextButton.gameObject.SetActive(false);
+        //     nextButtonFullScreen.gameObject.SetActive(false);
+        //     
+        //     switch (_position)
+        //     {
+        //         case GameConst.POS_UP:
+        //             container.transform.position = posUp.transform.position;
+        //             break;
+        //     }
+        //     
+        //     container.gameObject.SetActive(true);
+        //     container.transform.DOPunchScale(punchV, 0.23f);
+        //     await SetText(_text);
+        //     
+        //     await UniTask.Delay(500);
+        //
+        //     nextImage.gameObject.SetActive(_enableNext);
+        //     nextButton.gameObject.SetActive(_enableNext);
+        //     nextButtonFullScreen.gameObject.SetActive(_enableNext);
+        // }
+        //
+        // public void HideDialog()
+        // {
+        //     container.gameObject.SetActive(false);
+        // }
+        //
+        // [Button(ButtonSizes.Large), GUIColor(0.4f, 0.8f, 1)]
+        // public async UniTask SetText(string _text)
+        // {
+        //     if (dialogTextSequence.IsActive()) { dialogTextSequence.Kill(); }
+        //
+        //     StringBuilder dialogTextString = new StringBuilder(_text);
+        //
+        //     nextButtonFullScreen.interactable = false;
+        //     AudioManager.Ins.PlayTextSound(true);
+        //     
+        //     dialogText.text = _text;
+        //     dialogText.alpha = 0;
+        //     
+        //     DOTweenTMPAnimator animator = new DOTweenTMPAnimator(dialogText);
+        //
+        //     for (int i = 0; i < animator.textInfo.characterCount; ++i) 
+        //     {
+        //         if (!animator.textInfo.characterInfo[i].isVisible) continue;
+        //
+        //         // TODO: Make a pause
+        //         if (dialogText.textInfo.characterInfo[0].character == '_')
+        //         {
+        //             await UniTask.Delay(65);    
+        //         }
+        //         else
+        //         {
+        //             await UniTask.Delay(35);
+        //         }
+        //         
+        //         dialogTextSequence.Join(animator.DOFadeChar(i, 1, 0.05f));
+        //         dialogTextSequence.Join(animator.DOPunchCharScale(i, 0.33f, 0.23f).SetEase(Ease.InOutQuint));
+        //     }
+        //     
+        //     nextButtonFullScreen.interactable = true;
+        //     AudioManager.Ins.PlayTextSound(false);
+        // }
+        //
+        // public void OnNextClick()
+        // {
+        //     AudioManager.Ins.UIClickSound();
+        //     nextClickCallback.Invoke();
+        // }
 
-            nextImage.gameObject.SetActive(_enableNext);
-            nextButton.gameObject.SetActive(_enableNext);
-            nextButtonFullScreen.gameObject.SetActive(_enableNext);
-        }
-        
-        public void HideDialog()
-        {
-            container.gameObject.SetActive(false);
-        }
-        
-        [Button(ButtonSizes.Large), GUIColor(0.4f, 0.8f, 1)]
-        public async UniTask SetText(string _text)
-        {
-            if (dialogTextSequence.IsActive()) { dialogTextSequence.Kill(); }
-
-            StringBuilder dialogTextString = new StringBuilder(_text);
-
-            nextButtonFullScreen.interactable = false;
-            AudioManager.Ins.PlayTextSound(true);
-            
-            dialogText.text = _text;
-            dialogText.alpha = 0;
-            
-            DOTweenTMPAnimator animator = new DOTweenTMPAnimator(dialogText);
-
-            for (int i = 0; i < animator.textInfo.characterCount; ++i) 
-            {
-                if (!animator.textInfo.characterInfo[i].isVisible) continue;
-
-                // TODO: Make a pause
-                if (dialogText.textInfo.characterInfo[0].character == '_')
-                {
-                    await UniTask.Delay(65);    
-                }
-                else
-                {
-                    await UniTask.Delay(35);
-                }
-                
-                dialogTextSequence.Join(animator.DOFadeChar(i, 1, 0.05f));
-                dialogTextSequence.Join(animator.DOPunchCharScale(i, 0.33f, 0.23f).SetEase(Ease.InOutQuint));
-            }
-            
-            nextButtonFullScreen.interactable = true;
-            AudioManager.Ins.PlayTextSound(false);
-        }
-
-        public void OnNextClick()
-        {
-            AudioManager.Ins.UIClickSound();
-            nextClickCallback.Invoke();
-        }
-
-        public void SetNextClickCallback(UnityAction _callback)
-        {
-            nextClickCallback = _callback;
-        }
-
-        public override void OnViewCreated()
-        {
-            base.OnViewCreated();
-            container.gameObject.SetActive(false);
-            dialogTextSequence = DOTween.Sequence();
-
-            nextButton.onClick.AddListener(OnNextClick);
-            nextButtonFullScreen.onClick.AddListener(OnNextClick);
-        }
+        // public void SetNextClickCallback(UnityAction _callback)
+        // {
+        //     nextClickCallback = _callback;
+        // }
+        //
+        // public override void OnViewCreated()
+        // {
+        //     base.OnViewCreated();
+        //     container.gameObject.SetActive(false);
+        //     dialogTextSequence = DOTween.Sequence();
+        //
+        //     nextButton.onClick.AddListener(OnNextClick);
+        //     nextButtonFullScreen.onClick.AddListener(OnNextClick);
+        // }
     }
     
     

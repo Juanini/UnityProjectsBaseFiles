221aa049208089cd0a53e5b13088057ee26b4115
David Belmont <83025635+DavidBluestrats@users.noreply.github.com>
2023-09-02 12:47:00 -0600

    Funciones de Segfuir y No Seguir Minion

--- Scripts/CameraManager.cs
+++ Scripts/CameraManager.cs
@@ -9,13 +9,13 @@ using Sirenix.OdinInspector;
 public class CameraManager : MonoBehaviour
 {
     public static CameraManager Ins;
-    
+
     public Camera cam;
-    
+
     [BoxGroup("ProCamera2D")] public ProCamera2D proCamera2D;
     [BoxGroup("ProCamera2D")] public ProCamera2DPanAndZoom panAndZoom;
     [BoxGroup("ProCamera2D")] public ProCamera2DNumericBoundaries numericBoundaries;
-    
+
     void Awake()
     {
         Ins = this;
@@ -28,7 +28,7 @@ public class CameraManager : MonoBehaviour
             .SetEase(Ease.InOutExpo)
             .AsyncWaitForCompletion();
     }
-    
+
     // * =====================================================================================================================================
     // * 
 
@@ -38,30 +38,44 @@ public class CameraManager : MonoBehaviour
     {
         proCamera2D.enabled = false;
     }
-    
+
     public void EnableProCamera()
     {
         proCamera2D.enabled = true;
     }
-    
+
     public void DisablePanAndZoomScript()
     {
         panAndZoom.enabled = false;
     }
-    
+
     public void EnablePanAndZoomScript()
     {
         panAndZoom.enabled = true;
     }
-    
+
     public void DisableNumericBoundariesScript()
     {
         numericBoundaries.enabled = false;
     }
-    
+
     public void EnableNumericBoundariesScript()
     {
         numericBoundaries.enabled = true;
+    }
+
+    public void FollowMinion(Transform _minionTransform, float _zoomAmount, float _zoomSpeed)
+    {
+        DisableNumericBoundariesScript();
+        proCamera2D.Zoom(_zoomAmount, _zoomSpeed, EaseType.EaseInOut);
+        proCamera2D.AddCameraTarget(_minionTransform);
+    }
+
+    public void UnfollowMinions(Transform _minionTransform, float _zoomAmount, float _zoomSpeed)
+    {
+        EnableNumericBoundariesScript();
+        proCamera2D.Zoom(_zoomAmount, _zoomSpeed, EaseType.EaseInOut);
+        proCamera2D.RemoveCameraTarget(_minionTransform);
     }
 
     #endregion

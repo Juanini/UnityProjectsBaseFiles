b53c944087dadc849d45a91b53885367d27ce54c
Juan Vazquez <diclonius32@gmail.com>
2023-11-30 12:43:53 -0600

    * Action Blocker added.

---
+++ Scripts/ActionBlocker.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+
+public class ActionBlocker
+{
+    private bool canDoAction = true;
+    private int millisecondsToBlock = 200;
+    private CancellationTokenSource cancelToken;
+
+    public void Setup(int _milliseconds)
+    {
+        millisecondsToBlock = _milliseconds;
+    }
+    
+    public bool CanDoAction()
+    {
+        if (!canDoAction) return canDoAction;
+
+        cancelToken = new CancellationTokenSource();
+        canDoAction = false;
+        DoActionBlockTimer(cancelToken.Token);
+
+        return true;
+    }
+
+    public void Reset()
+    {
+        canDoAction = true;
+        cancelToken?.Cancel();
+    }
+
+    private async void DoActionBlockTimer(CancellationToken cancellationToken)
+    {
+        await UniTask.Delay(millisecondsToBlock);
+        canDoAction = true;
+    }
+}

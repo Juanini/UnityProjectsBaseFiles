8addc3706c78820ebc916cd82235539c4c8ae883
Juan Vazquez <diclonius32@gmail.com>
2023-12-02 13:07:20 -0600

    * Singleton Class added.

---
+++ Scripts/Singleton.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T ins;
+    public static T Ins
+    {
+        get
+        {
+            if (ins == null)
+            {
+                ins = (T)FindObjectOfType(typeof(T));
+                if (ins == null)
+                {
+                    SetupInstance();
+                }
+            }
+
+            return ins;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        RemoveDuplicates();
+    }
+    
+    private static void SetupInstance()
+    {
+        ins = (T)FindObjectOfType(typeof(T));
+        if (ins == null)
+        {
+            GameObject gameObj = new GameObject();
+            gameObj.name = typeof(T).Name;
+            ins = gameObj.AddComponent<T>();
+            DontDestroyOnLoad(gameObj);
+        } 
+    }
+
+    private void RemoveDuplicates()
+    {
+        if (ins == null)
+        {
+            ins = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+    }
+}

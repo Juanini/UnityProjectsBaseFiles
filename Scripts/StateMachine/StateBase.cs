4f5d15d7381d8152c65d169455459fd00c2f6f3e
Juan Vazquez <diclonius32@gmail.com>
2024-04-16 11:17:31 -0600

    * Naming changes

--- Scripts/StateMachine/StateBase.cs
+++ Scripts/StateMachine/StateBase.cs
@@ -8,16 +8,16 @@ public abstract class StateBase<EState> where EState : Enum
         StateKey = _key;
     }
 
-    protected object mainClassRef;
+    protected object context;
     
     public void InitState<T>(T _classReference) where T : class
     {
-        mainClassRef = _classReference;
+        context = _classReference;
     }
     
-    public T GetMainClassReference<T>() where T : class
+    public T GetContext<T>() where T : class
     {
-        return mainClassRef as T;
+        return context as T;
     }
     
     public EState StateKey { get; private set; }

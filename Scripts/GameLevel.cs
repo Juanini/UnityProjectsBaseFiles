90ad81c668dde76fd9ea086fd5f1f771bc5eae4c
Juan Vazquez <diclonius32@gmail.com>
2025-07-08 15:32:58 -0600

    + Game Level script added.

---
+++ Scripts/GameLevel.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class GameLevel : MonoBehaviour
+{
+    [BoxGroup("Assets")] public Transform assetsParent;
+    [BoxGroup("Assets")] public List<AssetReference> assetsToLoad;
+    
+    void Start()
+    {
+        OnSceneLoaded();
+        GameManager.Ins.OnLevelLoaded(this);
+    }
+
+    public virtual void OnSceneLoaded() { }
+    public virtual void OnSceneBeforeBeingUnloaded() { }
+
+    protected async UniTask LoadAssets()
+    {
+        foreach (var assetToLoad in assetsToLoad)
+        {
+            var obj = await AssetLoader.Ins.InstantiatePrefabAsync(assetToLoad, assetsParent);
+
+            if (obj != null)
+            {
+                Trace.Log(this.name + " - " + "LOAD ASSET: " + obj.name);
+            }
+        }
+    }
+}

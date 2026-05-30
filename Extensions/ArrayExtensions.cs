1b80e282810e11825f77bcb32311c604412eb7e1
Juan Vazquez <diclonius32@gmail.com>
2026-05-29 18:29:51 -0600

    * Shuffle extension added.

--- Extensions/ArrayExtensions.cs
+++ Extensions/ArrayExtensions.cs
@@ -26,4 +26,13 @@ public static class ArrayExtensions
         
         return list[Random.Range(0, list.Count)];
     }
+    
+    public static void Shuffle<T>(this List<T> list)
+    {
+        for (int i = list.Count - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1); // UnityEngine.Random, max is exclusive
+            (list[i], list[j]) = (list[j], list[i]); // swap using tuple deconstruction
+        }
+    }
 }

b56d4effd13ace14e86320dc53ea58a573f5d99c
Juan Vazquez <diclonius32@gmail.com>
2026-06-19 13:16:33 -0600

    * Singleton Fixes.

--- Scripts/Singleton.cs
+++ Scripts/Singleton.cs
@@ -10,10 +10,6 @@ public class Singleton<T> : MonoBehaviour where T : Component
             if (ins == null)
             {
                 ins = (T)FindObjectOfType(typeof(T));
-                if (ins == null)
-                {
-                    SetupInstance();
-                }
             }
 
             return ins;
@@ -22,29 +18,12 @@ public class Singleton<T> : MonoBehaviour where T : Component
 
     public virtual void Awake()
     {
-        RemoveDuplicates();
-    }
-    
-    private static void SetupInstance()
-    {
-        ins = (T)FindObjectOfType(typeof(T));
-        if (ins == null)
-        {
-            GameObject gameObj = new GameObject();
-            gameObj.name = typeof(T).Name;
-            ins = gameObj.AddComponent<T>();
-        } 
-    }
-
-    private void RemoveDuplicates()
-    {
-        if (ins == null)
-        {
-            ins = this as T;
-        }
-        else 
+        if (ins != null && ins != this)
         {
             Destroy(gameObject);
+            return;
         }
+
+        ins = this as T;
     }
 }
